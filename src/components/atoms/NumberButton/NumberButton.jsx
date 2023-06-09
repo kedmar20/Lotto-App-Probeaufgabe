afc39bcafc68ae029157a20ba7cb09db1a6ebab6
Kedmar20 <m.kedzierski76@gmail.com>
2023-06-09 18:01:21 +0000

    add numbers

--- src/components/atoms/NumberButton/NumberButton.jsx
+++ src/components/atoms/NumberButton/NumberButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function NumberButton() {
+export default function NumberButton({number}) {
   return (
     <div>
-      11
+      {number}
     </div>
   )
 }
