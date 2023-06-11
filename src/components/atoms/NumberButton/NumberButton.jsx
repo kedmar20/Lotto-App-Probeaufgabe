d8dd21cd6cf0ea52059d9be6a1a2d7c6fa64b93b
Kedmar20 <m.kedzierski76@gmail.com>
2023-06-11 05:23:20 +0000

    add NumberProvider, Context, buttonValue state

--- src/components/atoms/NumberButton/NumberButton.jsx
+++ src/components/atoms/NumberButton/NumberButton.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
 import { NumberButtonStyles } from './NumberButton.styles'
+import { NumbersContext } from "src/providers/Providers";
+
+
 
 export default function NumberButton({number}) {
+  const context = useContext(NumbersContext);
+
+const [buttonValue, setButtonValue] = useState(number);
+
+const AddNumber = () => {
+   console.log(context.numbers);
+   console.log('clicked');
+   setButtonValue('X');
+   if (buttonValue==number) {
+    context.handleAddNumber(number);
+  } else {
+    context.deleteNumber(number);
+  }
+  console.log(context.numbers);
+}
+
+// const buttonValue = ()=>{
+//   if 
+// return (number)
+// }
+
   return (
-    <NumberButtonStyles>
-      {number}
+    <NumberButtonStyles onClick={()=>AddNumber()}>
+      {buttonValue}
     </NumberButtonStyles>
   )
 }
