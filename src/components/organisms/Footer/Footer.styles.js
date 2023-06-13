dbd070e4d07a57227caaba09f308a7d19d7bfcee
Kedmar20 <m.kedzierski76@gmail.com>
2023-06-13 19:30:21 +0000

    add icon trash, style buton weiter

--- src/components/organisms/Footer/Footer.styles.js
+++ src/components/organisms/Footer/Footer.styles.js
@@ -1,15 +1,41 @@
 import styled from "styled-components";
 
 export const FooterStyles = styled.div`
-    display: flex;
-    flex-wrap:wrap;
-    justify-content: space-between;
-    padding: 0 20px;
-    align-items: start;
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: start;
+   padding: 0 20px;
+   align-items: start;
    width: 100%;
    gap: 5px;
    color: ${({ theme }) => theme.colors.textHeader};
    font-size: 1.3em;
    position: absolute;
-   bottom: 20px; 
+   bottom: 20px;
+
+   img {
+      width: 3vw;
+      color: ${({ theme }) => theme.colors.textHeader};
+   }
+
+   span {
+      width: 13vw;
+   }
+
+   button {
+      width: 13vw;
+      position: absolute;
+      right: 5%;
+      background-color: ${({ theme }) => theme.colors.cross};
+      color: ${({ theme }) => theme.colors.white};
+      border-radius: 25px;
+      border: none;
+      padding: 6px 0;
+      transition: all 0.3s ease-in-out;
+      cursor: pointer;
+
+      &:hover {
+         transform: scale(1.1);
+      }
+   }
 `;
