b9f1ea306cf516c45b028d82d37a77ba922a3887
Kedmar20 <m.kedzierski76@gmail.com>
2023-06-13 18:08:43 +0000

    improve functionality of button 'weiter'

--- src/components/atoms/NumberButton/NumberButton.jsx
+++ src/components/atoms/NumberButton/NumberButton.jsx
@@ -4,31 +4,23 @@ import { NumbersContext } from "src/providers/Providers";
 
 export default function NumberButton({ number, i }) {
    const context = useContext(NumbersContext);
-
    const [buttonValue, setButtonValue] = useState(number);
 
+   const cross = () => <img src="src/assets/icons/cross.svg" />;
+
    const AddNumber = (i) => {
-      // console.log(i);
       console.log(context.numbers.length);
 
       if (context.numbers.length < 6) {
-         context.numbers.length >= 5 ? context.setToggle(true) : "";
-         // context.numbers.length >= 5 ? context.setToggle(true) : context.setToggle(false);
          if (buttonValue == number) {
-            setButtonValue("X");
-            // context.setNumbers([number, ...context.numbers]);
+            setButtonValue(cross);
             context.handleAddNumber(number);
          } else {
             context.deleteNumber(number);
             setButtonValue(number);
-            context.setToggle(false);
          }
       } else {
          context.deleteNumber(number);
-         //  context.setToggle(false);
-
-         context.numbers.length <= 6 && buttonValue == "X" ? context.setToggle(false) : "";
-         //  context.numbers.length <= 5 ? context.setToggle(false) : "";
          setButtonValue(number);
          return;
       }
